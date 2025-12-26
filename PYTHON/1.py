aab2fb553b62120eaa846dc990afe146cfad70ab
Gokula Krishna M <gokulakrishna2110@gmail.com>
2025-12-26 12:50:41 +0530

    Data Types

---
+++ PYTHON/1.py
@@ -0,0 +1,11 @@
+#Create 4 variables of different data types
+#(integer, float, string, boolean) that describe yourself (age, height, name, are_you_student).
+# Print all of them with nice formatting.
+name=input("name")
+age=int(input("age"))
+height=float(input("height"))
+are_you_student=bool(input("are you student?"))
+print ("Name :", name)
+print ("Age :", age)
+print("Height :", height)
+print("You are a student ",are_you_student)
