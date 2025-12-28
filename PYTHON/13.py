874c3af697eb24ab3f5f99cb06d795dc0baa4f34
Gokula Krishna M <gokulakrishna2110@gmail.com>
2025-12-29 01:13:37 +0530

    List

---
+++ PYTHON/13.py
@@ -0,0 +1,8 @@
+#Create a list with 6 different food names.
+#Use a for loop to print each food with numbering:
+#1. Pizza
+#2. Sushi
+#3. ...
+foods=["Pizza","Sushi","Tacos","Burger","Pasta","Ice cream"]
+for i in range (0,6):
+    print(i+1,".",foods[i])
