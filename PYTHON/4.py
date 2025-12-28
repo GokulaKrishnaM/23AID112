c77d45f770d675a8dcb25ff3b5c259a85828e6e5
Gokula Krishna M <gokulakrishna2110@gmail.com>
2025-12-28 15:28:06 +0530

    Even/Odd

---
+++ PYTHON/4.py
@@ -0,0 +1,7 @@
+#Create a variable with your favourite number.
+#Print whether this number is even or odd (use % operator)
+fav_num=int(input("Enter your favourite number"))
+if (fav_num%2==0):
+    print("Your favourite number is an even number")
+else:
+    print("Your favourite number is an odd number")
