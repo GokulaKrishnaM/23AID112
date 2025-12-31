4aa7df22f2538895df8507947330886c0eb9c086
Gokula Krishna M <gokulakrishna2110@gmail.com>
2025-12-31 11:56:04 +0530

    Update 18.py

--- PYTHON/18.py
+++ PYTHON/18.py
@@ -3,15 +3,15 @@
 #Let user guess (use while loop) until they guess correctly.
 #After each wrong guess print "Too high" or "Too low".
 Secret_number=7
-
-while (i!=Secret_number):
-    guess_number=int(input("guess a number"))
-    if (i>Secret_number):
+guess_number=int(input("guess a number:"))
+while (guess_number!=Secret_number):
+    
+    if (guess_number>Secret_number):
         print("Too high")
         
-    elif (i<Secret_number):
+    elif (guess_number<Secret_number):
         print("Too Low")
     else:
-        print("Yes the answer is correct!")
-    
-
+        break
+    guess_number=int(input("guess a number:"))
+print("Yes the answer is correct!")
